8ccd28b384c0001f0d5b4e8ad4ceed626c04be19
Dan Ko <danko56666@gmail.com>
2024-11-26 22:08:41 -0500

    feat: install tailwind

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,5 +3,5 @@
 // TODO: replace with actual button component
 
 export default function Button() {
-  return <button>Click Me</button>;
+  return <button className="bg-red-600">Click Me</button>;
 }
