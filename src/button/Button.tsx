e77187f14e41680a331b85a67871a404a6cb8740
Jane Moroz <Moroz.jane@yahoo.com>
2024-12-01 15:21:41 +0300

    chore: set up storybook with tailwind

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,7 +1,7 @@
-"use client";
-
-// TODO: replace with actual button component
-
 export default function Button() {
-  return <button className="bg-red-600">Click Me</button>;
+  return (
+    <button className="p-10 uppercase bg-pink-300 border-blue-300 border-spacing-1">
+      Click Me
+    </button>
+  );
 }
