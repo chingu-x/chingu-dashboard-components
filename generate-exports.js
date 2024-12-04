49df975f0b70b4e025fbeaf1c2eac10b43d163fd
Dan Ko <danko56666@gmail.com>
2024-12-03 20:14:32 -0500

    feat: setup tailwind config that makes tailwind styles from package recognized

--- generate-exports.js
+++ generate-exports.js
@@ -3,7 +3,6 @@ import path from "path";
 
 // Define the root path of your dist and types folders
 const distPath = "./dist";
-const typesPath = "./dist/types";
 
 // Read all directories in the components folder
 const components = fs.readdirSync(distPath).filter((file) => {
@@ -15,6 +14,9 @@ const exportsObject = {
   ".": {
     import: "./dist/index.js",
     types: "./dist/types/index.d.ts",
+  },
+  "./styles": {
+    import: "./dist/style.css",
   },
 };
 
