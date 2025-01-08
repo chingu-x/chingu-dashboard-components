3bf7c92efa59e0d7a4a82346ed5c361b4d8e9b96
Jane Moroz <Moroz.jane@yahoo.com>
2025-01-08 13:46:38 +0300

    feat: add badge (refactored)

--- src/badge/Badge.tsx
+++ src/badge/Badge.tsx
@@ -1,7 +1,48 @@
-"use client";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "../tw-merge";
 
-// TODO: replace with actual badge component
+const badge = cva(
+  "flex items-center justify-center overflow-hidden rounded-[100px] font-medium text-base-300",
+  {
+    variants: {
+      variant: {
+        primary: ["bg-accent-content"],
+        error: ["bg-error-content"],
+        warning: ["bg-warning-content"],
+        success: ["bg-success-content"],
+      },
+      size: {
+        sm: ["py-[2px]", "px-[8px]", "text-[13px]", "gap-x-[4px]", "h-[20px]"],
+        md: ["py-[3px]", "px-[9px]", "text-base", "gap-x-[5px]", "h-[25px]"],
+        lg: ["py-[4px]", "px-[10px]", "text-xl", "gap-x-[6px]", "h-[32px]"],
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+    },
+  }
+);
 
-export default function Badge() {
-  return <div>ExampleComponent</div>;
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badge> {
+  title: string;
+  children?: React.ReactNode;
+}
+
+export default function Badge({
+  className,
+  title,
+  children,
+  variant,
+  size,
+  ...props
+}: BadgeProps) {
+  return (
+    <div className={cn(badge({ variant, size, className }))} {...props}>
+      {children}
+      <span>{title}</span>
+    </div>
+  );
 }
