b037e921c8950c545b6f916f1a709084457d9592
Dan Ko <danko56666@gmail.com>
2025-01-15 16:54:35 -0500

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/**/*"],
+  },
+];
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslintPlugin()],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
