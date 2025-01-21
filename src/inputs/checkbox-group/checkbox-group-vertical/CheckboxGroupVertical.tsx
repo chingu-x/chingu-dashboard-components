5b481f211a819df69fde80d3ef9e3cc7c04de445
Dan Ko <danko56666@gmail.com>
2025-01-21 02:38:05 -0500

    fix: checkbox group vertical export

--- src/inputs/checkbox-group/checkbox-group-vertical/CheckboxGroupVertical.tsx
+++ src/inputs/checkbox-group/checkbox-group-vertical/CheckboxGroupVertical.tsx
@@ -3,28 +3,29 @@ import CheckboxInput, {
   type CheckboxInputProps,
 } from "../../checkbox-input/CheckboxInput";
 
-interface CheckboxGroupProps
+interface CheckboxGroupVerticalProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   options: CheckboxInputProps[];
 }
 
-const CheckboxGroup = React.forwardRef<HTMLInputElement, CheckboxGroupProps>(
-  ({ options, ...props }, ref) => (
-    <div className="flex flex-col gap-y-5">
-      {options.map(({ id, value, label }) => (
-        <CheckboxInput
-          key={id}
-          id={id}
-          value={value}
-          label={label}
-          {...props}
-          ref={ref}
-        />
-      ))}
-    </div>
-  ),
-);
+const CheckboxGroupVertical = React.forwardRef<
+  HTMLInputElement,
+  CheckboxGroupVerticalProps
+>(({ options, ...props }, ref) => (
+  <div className="flex flex-col gap-y-5">
+    {options.map(({ id, value, label }) => (
+      <CheckboxInput
+        key={id}
+        id={id}
+        value={value}
+        label={label}
+        {...props}
+        ref={ref}
+      />
+    ))}
+  </div>
+));
 
-CheckboxGroup.displayName = "CheckboxGroup";
+CheckboxGroupVertical.displayName = "CheckboxGroupVertical";
 
-export default CheckboxGroup;
+export default CheckboxGroupVertical;
